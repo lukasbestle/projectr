7a3fe21f3d7999f9c1f959ecc268c6bb0770d194
Lukas Bestle <lukas@lu-x.me>
2014-09-12 20:38:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# Project management tools
+
+> Tools for deployments and project management
+
+## What this does
+
+[Uberspace](https://uberspace.de) has a great routing setup for Apache sites: You place a directory or symbolic link with the name of the domain (like `www.example.com`) in `/var/www/virtual/<user>/` and every request to this domain is routed to the directory.
+
+As a user with a [pretty complex routing setup](https://git.lukasbestle.com/groups/sites), I wanted to automate setting up new sites and updating them automatically from a Git repository. This toolset provides some CLI tools to make that possible.
+
+## Features
+
+- Create and delete projects (general) and sites (projects for the webserver)
+- Set an origin Git repository and branch to get new versions from
+- Create and delete links from one or multiple domains to a site
+- Get a specific new revision from the Git repository, run a setup script (`.postdeploy.sh` in the repository root) and point a link to the new version
+- Reverse a deployable project to the last version
+
+### Directory structure of a project
+
+Every project/site follows this directory structure:
+
+	.branch  # Branch to allow deployments for (see "Deployment setup")
+	.domains # Site-specific: List of linked domains for this site
+	.origin  # URL of the origin repository
+	.project # Empty file determining that this is a project
+	current  # Symlink to the currently active version in versions/
+	├── <project files>
+	data     # Never directly accessible by user agents and never overwritten
+	├── <persistent non-VCS application data and configuration>
+	last     # Symlink to the last active version in versions/
+	├── <project files>
+	logs     # Logs of all deployments
+	├── <commit-hash>.log
+	├── <commit-hash>.log
+	versions # Fully separate Git repositories of the project
+	├── <id>-<commit-hash>
+	│   └── <project files>
+	└── <id>-<commit-hash>
+	    └── <project files>
+
+### Usage without any kind of deployment
+
+If you don't want to keep old versions of your code and use automatic deployment, simply leave out the `<origin>` parameter and you end up with a structure like this:
+
+	.domains # Site-specific: List of linked domains for this site
+	.project # Empty file determining that this is a project
+	current  # Directory for your project files
+	├── <project files>
+	data     # Never directly accessible by user agents and never overwritten
+	└── <persistent non-VCS application data and configuration>
+
+### Deployment setup
+
+The tool `project_deploy` takes the full path to the project and the Git revision to install. Obviously, this is not very useful, but easy to use in deployment hook scripts:
+
+1. Write a script that receives web-hooks from GitHub, GitLab or similar and get the repository URL, commit SHA-1 and branch name of the event from the transmitted data.
+2. Read the file `~/.projects`, which contains the paths to all known projects and sites, and iterate through it.
+3. Open the projects `.origin` and `.branch` files. If they match the web-hook, run `project_deploy <path> <commit-sha1>` and you are done.
+
+You can find an example PHP script for GitLab webhooks in `webhook.gitlab.php`. Another one for GitHub might follow, but it should be easy to do.
+
+## Setup
+
+1. Put this project wherever you want on the destination system and add the `bin` directory to your `PATH`.
+2. Create a backup and clean your `DocumentRoot` (`/var/www/virtual/<user>/` on Uberspace), as you probably want to manage everything with this toolset.
+3. Create a symlink to your `DocumentRoot` in `~/web` (this is what the `site_*` tools use):  
+   `ln -s /var/www/virtual/$USER/ ~/web`
+   
+   You can also link to a subdirectory of your `DocumentRoot` if you want to manage the sites of this tool separately from your other sites. Please note that the `site_link` functionality does not work without manually linking the resulting links to your `DocumentRoot` when using non-standard paths for `~/web`.
+4. Have fun with the tools in `bin`.
+
+## Configuration
+
+If you want to customize specific settings, you can create a Bash file at `~/.project.cnf` overriding the default values at every run of the tools. These are the possible settings and also the format of the file:
+
+	# Default branch to set if no one is given to `project_origin`
+	CONFIG_DEFAULT_BRANCH="master"
+	
+	# Allowed length of the <revision> parameter of `project_deploy`
+	# Used to make deployments consistent (doesn't allow different hash lengths and therefore duplicated deployments)
+	# Default is a full-length SHA-1 hash, use 7 as value when using short hashes.
+	CONFIG_HASH_LENGTH=40
+	
+	# Number of deployed versions to preserve (0 for infinite (be careful, that might use loads of storage space!))
+	# Versions older than the latest n versions get deleted automatically, logs are always preserved
+	CONFIG_PRESERVE_VERSIONS=5
+
+## Author
+
+- Lukas Bestle <mail@lukasbestle.com>
+
+## License
+
+This project was published under the terms of the MIT license. You can find a copy [over at the repository](https://git.lukasbestle.com/tools/misc/blob/master/LICENSE.md).
---
+++ webhook.gitlab.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * Project management tools
+ * Tools for deployments and project management
+ * 
+ * GitLab frontend for deployments
+ * 
+ * @author Lukas Bestle <mail@lukasbestle.com>
+ * @copyright Copyright 2014 Lukas Bestle
+ * @file webhook.gitlab.php
+ */
+
+// 1. Configuration
+// ====================
+
+// Long token used as authentication (appended as GET parameter: https://hooks.example.com/webhook.gitlab.php?token=<AUTH_TOKEN>)
+// ATTENTION: Make sure this is long and secure, as anyone could checkout any commit of your projects otherwise
+define('AUTH_TOKEN', '<long token>');
+
+// Path where you installed the scripts from the "bin" directory of this repository
+// This is required, as PHP doesn't automatically use the environment and therefore your $PATH from your shell
+define('TOOLKIT_PATH', '/home/<user>/bin');
+
+// 2. Setup
+// ====================
+
+/**
+ * This is an example webhook for GitLab servers.
+ * It's fairly simple to set this script up on your server/Uberspace, here's how:
+ * 
+ * 1. Setup the toolkit, so you can access the tools from your SSH shell (more about that in README.md)
+ * 2. Take a look at the settings above.
+ * 	2.1. Seriously, have you set an auth token? Yes? Alright.
+ * 3. Place this script in a directory in ~/web/ (something like "~/web/hooks.example.com")
+ * 4. You should now be able to access https://hooks.example.com/webhook.gitlab.php?token=<AUTH_TOKEN>
+ *    This should output "Authenticated, but no POST body sent.".
+ * 5. Generate an SSH key by using `ssh-keygen`. The default values are alright for this purpose.
+ * 6. Copy the contents of ~/.ssh/id_rsa.pub to your clipboard and add it as "Deploy Key" in your project's GitLab settings interface.
+ * 
+ * Then, you can easily create projects and let this script deploy them for you:
+ * 
+ * 7. Create the project on the server by running either `project_add <path> <exact SSH url from GitLab>#<branch to deploy>` or
+ *                                                       `site_add    <name> <exact SSH url from GitLab>#<branch to deploy>`.
+ *    It's important to use the exact SSH url (not the HTTPS one), so this script can find the project!
+ * 8. Add the URL (the one you tested in 4.) of the script to your project's web hooks.
+ * 9. Hit "Test Hook" in GitLab's settings interface. The project should be deployed a few seconds later.
+ * 
+ * Troubleshooting:
+ *  - Use `cat <path to project>/logs/*.log`. This should tell you what went wrong.
+ *  - Check if the TOOLKIT_PATH you set above is correct.
+ *  - Check if you set the origin of your project to the correct URL.
+ *  - Open the web hook URL you entered in GitLab in your browser and check if the output is "Authenticated, but no POST body sent.".
+ *  - If there is no log file and you can't find the reason yourself, it is complicated. ;)
+ *    Debugging this is not easy (GitLab does not seem to store logs of its requests), but you can try to craft a JSON body like GitLab,
+ *    send it to this script by using cURLor GUI tools like Rested and it will tell you exactly what went wrong.
+ */
+
+// 3. Done
+// The code starts here
+// ====================
+
+// We are always returning plain text
+header('Content-Type: text/plain');
+
+// Check if the authentication is valid
+if(!isset($_GET['token']) || $_GET['token'] !== AUTH_TOKEN) {
+	http_response_code(401);
+	die('Invalid authentication.');
+}
+
+// Get the request body
+$inputPointer = fopen('php://input', 'r');
+$input = stream_get_contents($inputPointer);
+if(!$input) {
+	http_response_code(405);
+	die('Authenticated, but no POST body sent.');
+}
+
+// Parse payload
+$payload = json_decode($input, true);
+if(!is_array($payload)) {
+	http_response_code(400);
+	die('Invalid payload (no JSON?).');
+}
+
+// Get some interesting information from the payload
+$url    = $payload['repository']['url'];
+$commit = $payload['after'];
+$ref    = $payload['ref'];
+if(!preg_match('{(?:.*/){2}(.*)}', $ref, $matches)) {
+	http_response_code(400);
+	die('Invalid ref field (does not match regular expression "(?:.*/){2}(.*)").');
+}
+$branch = $matches[1];
+
+// Debug
+echo "Received commit hash \"$commit\" for repository URL \"$url\" (branch \"$branch\").\n";
+
+// Open ~/.projects and iterate through every project
+$listPointer = fopen($_SERVER['HOME'] . '/.projects', 'r');
+$exitCode = 0;
+while(($project = fgets($listPointer)) !== false) {
+	// Trim whitespace
+	$project = trim($project);
+	
+	// Only deployable projects are interesting for us
+	if(!is_file($project . '/.origin') || !is_file($project . '/.branch')) continue;
+	
+	// If there is a .origin and .branch file, check if they match
+	if(file_get_contents($project . '/.origin') == $url && file_get_contents($project . '/.branch') == $branch) {
+		// Found the right project
+		echo "Found project at $project, running deploy script.\n";
+		
+		// Run deploy script
+		passthru('export PATH=' . escapeshellarg(TOOLKIT_PATH) . ':$PATH; project_deploy ' . escapeshellarg($project) . ' ' . escapeshellarg($commit), $exitCode);
+		
+		// If it didn't work, add debug statement
+		if($exitCode !== 0) echo "Something didn't work.\n";
+	}
+}
+
+// Iterated through every project, exit
+http_response_code(($exitCode === 0)? 200 : 500);
+die('All done.');
